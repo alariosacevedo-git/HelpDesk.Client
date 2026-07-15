d77f52d33006fef706c91389e4179fa306f3e83a
Alejandro Larios Acevedo <alariosacevedo@gmail.com>
2026-07-15 15:27:45 -0700

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace HelpDesk.Client.UI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
