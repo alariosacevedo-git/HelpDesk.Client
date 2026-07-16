28fc671254a32938a51537815295d2bf075cdec8
Alejandro Larios Acevedo <alariosacevedo@gmail.com>
2026-07-16 10:16:10 -0700

    Add files via upload

---
+++ HelpDesk.Client.UI/wwwroot/scripts/DashBoard.js
@@ -0,0 +1,14 @@
+﻿var _DashBoard = {
+    onMainMenuActive: function (LiParent, LiChild) {
+        var liParent = LiParent;
+        var liChild = LiChild;
+
+        if (liParent != "Null") {
+            $("#"+ LiParent +"").addClass("active open");
+        }
+
+        if (LiChild != "Null") {
+            $("#" + liChild + "").addClass("active");
+        }
+    }
+};
