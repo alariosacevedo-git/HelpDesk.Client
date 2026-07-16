dc0a357a0b984f4fed74c74a63fa4a779237e1b9
Alejandro Larios Acevedo <alariosacevedo@gmail.com>
2026-07-16 10:04:07 -0700

    Add files via upload

---
+++ HelpDesk.Client.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HelpDesk.Client.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
